1ca13371fef9ab886940fb2a9fcdd2e9ae96d241
trung <>
2021-12-28 21:54:39 +0700

    Implement script for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,13 @@ import '@nomiclabs/hardhat-web3'
 
 import { getDeployParams } from './scripts/utils'
 
+const PRIVATE_KEY_DEPLOYER_ACCOUNT =
+  process.env.PRIVATE_KEY_DEPLOYER_ACCOUNT
+
+const accounts = [
+  PRIVATE_KEY_DEPLOYER_ACCOUNT, 
+];
+
 module.exports = {
   defaultNetwork: 'hardhat',
   solidity: {
@@ -25,18 +32,44 @@ module.exports = {
       },
     ],
   },
+  // networks: {
+  //   hardhat: {
+  //     blockGasLimit: 10000000,
+  //     gas: 10000000,
+  //   },
+  //   local: {
+  //     url: 'http://127.0.0.1:8545',
+  //     blockGasLimit: 10000000,
+  //     gas: 10000000,
+  //     network_id: '*', // eslint-disable-line camelcase
+  //   },
+  //   deploy: getDeployParams(),
+  // },
   networks: {
     hardhat: {
-      blockGasLimit: 10000000,
-      gas: 10000000,
+      forking: {
+        enabled: process.env.FORKING_ENABLED === "true" ? true : false,
+        url: "https://rpc-mumbai.maticvigil.com",
+      },
     },
-    local: {
-      url: 'http://127.0.0.1:8545',
-      blockGasLimit: 10000000,
-      gas: 10000000,
-      network_id: '*', // eslint-disable-line camelcase
+    maticmainnet: {
+      url: "https://matic-mainnet.chainstacklabs.com", // https://rpc-mainnet.maticvigil.com
+      accounts,
+      chainId: 137,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 111000000000,
     },
-    deploy: getDeployParams(),
+    matictestnet: {
+      url: "https://matic-mumbai.chainstacklabs.com", // https://rpc-mumbai.maticvigil.com
+      accounts,
+      chainId: 80001,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 3000000000, // 3 Gwei
+      gasMultiplier: 1,
+    },
   },
   gasReporter: {
     chainId: 1,
